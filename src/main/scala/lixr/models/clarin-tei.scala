e262db9b4c5d9a8cff8c1120e0e4b38b739a40cf
John McCrae <john@mccr.ae>
2015-03-20 00:13:32 +0100

    develop clarin models

---
+++ src/main/scala/lixr/models/clarin-tei.scala
@@ -0,0 +1,95 @@
+package eu.liderproject.lixr.models
+
+import eu.liderproject.lixr._
+
+object ClarinTEI extends Clarin {
+  val ms = Namespace("http://purl.org/ms-lod/MetaShare.ttl#")
+  val tei = cmd
+
+  cmd.Components --> handle(cmd.teiHeader)
+
+  cmd.teiHeader --> (
+  	handle(cmd.fileDesc),
+  )
+
+  cmd.fileDesc --> (
+  	handle(cmd.titleStmt)
+  )
+
+  cmd.titleStmt --> (
+  	tei.titleStmt > node(frag("titleStmt")) (
+  	  handle(cmd.title),
+  	  handle(cmd.author),
+  	  handle(cmd.editor),
+  	)
+  )
+
+  cmd.title --> (
+  	tei.title > node(frag("title")) (
+  	  a > tei.TitleStmt,
+  	  rdf.value > content,
+  	  stringAttMap(current, "type", tei.titleType),
+  	  stringAttMap(current, "level", tei.titleLevel),
+  	  intAttMap(current, "n", tei.titleN)
+  	)
+  )
+
+  cmd.author --> (
+  	tei.author > node(frag("author")) (
+  	  a > foaf.Person,
+  	  handle(cmd.persName),
+  	)
+  )
+
+  cmd.persName --> (
+  	stringMap(cmd.surname, foaf.familyName),
+  	stringMap(cmd.forename, foaf.givenName),
+  	stringMap(cmd.nameLink, tei.nameLink),
+  	stringMap(cmd.genName, tei.genName),
+  	stringMap(cmd.addName, tei.addName),
+  	stringMap(cmd.roleName, tei.roleName),
+  	forall(cmd.idno) (
+  	  handle(cmd.idno)
+  	)
+  )
+
+  cmd.idno --> (
+  	when(att("type") === "DTAID")(
+  	  tei.DTAID > content
+  	).or(att("type") === "URN")(
+  	  tei.URN > content
+	).or(att("type") === "shelfmark")(
+	  tei.shelfmark > content
+	).or(att("type") === "DTADirName")(
+	  tei.DTADirName > content
+	).or(att("type") === "URLCatalogue")(
+	  tei.URLCatalogue > content
+	).or(att("type") === "URLImages")(
+	  tei.URLImages > content
+	).or(att("type") === "URLWeb")(
+	  tei.URLWeb > content
+	).or(att("type") === "URLXML")(
+	  tei..URLXML > content
+	).or(att("type") === "URLText")(
+	  tei.URLText > content
+	).or(att("type") === "URLHTML")(
+	  tei.URLHTML > content
+	).or(att("type") === "URLCAB")(
+	  tei.URLCAB > content
+	).or(att("type") === "URLTCF")(
+	  tei.URLTCF > content
+	).or(att("type") === "PND")(
+	  tei.PND > content
+	).or(att("type") === "PIDCMDI")(
+	  tei.PIDCMDI > content
+	)
+  )
+
+  cmd.editor --> (
+  	mods.editor > node(frag("editor")) (
+  	  a > foaf.Person,
+  	  handle(cmd.persName),
+  	  stringMap(cmd.role, tei.role)
+  	)
+  )
+}
